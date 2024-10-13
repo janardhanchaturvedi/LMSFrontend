08eeac0c817ad7169a900c14ed018006aca114d1
Janardhan <jan.cj84@gmail.com>
2024-10-13 21:30:24 +0530

    Feat(login) : Added the Role Dropdown

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -14,6 +14,7 @@ function Signup() {
     email: "",
     password: "",
   });
+  console.log("🚀 ~ Signup ~ loginData:", loginData)
   function handleUserInput(e) {
     const { name, value } = e.target;
     setLoginData({
@@ -59,7 +60,7 @@ function Signup() {
               name="email"
               id="email"
               placeholder="Enter your email.."
-              className="bg-transparent px-2 py-1 border"
+              className="bg-transparent px-2 py-1 border rounded-lg"
               onChange={handleUserInput}
               value={loginData.email}
             />
@@ -75,10 +76,29 @@ function Signup() {
               name="password"
               id="password"
               placeholder="Enter your password.."
-              className="bg-transparent px-2 py-1 border"
+              className="bg-transparent px-2 py-1 border rounded-lg"
               onChange={handleUserInput}
               value={loginData.password}
             />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label
+              htmlFor="password"
+              className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              {" "}
+              Role{" "}
+            </label>
+            <select
+              id="role"
+              name="role"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={handleUserInput}
+            >
+              <option selected>Choose a Role</option>
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+            </select>
           </div>
 
           <button
