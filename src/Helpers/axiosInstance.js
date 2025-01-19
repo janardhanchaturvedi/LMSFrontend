cfd54df965d259fb1d9ab0247a32f86b85bafd6b
Janardhan <jan.cj84@gmail.com>
2025-01-19 11:43:32 +0530

    add update base url

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "https://lmsbackend-3etv.onrender.com/api/v1";
+// const BASE_URL = "https://lmsbackend-3etv.onrender.com/api/v1";
+const BASE_URL = "http://51.20.183.198/api/v1";
+
+
 
 const axiosInstance = axios.create();
 
